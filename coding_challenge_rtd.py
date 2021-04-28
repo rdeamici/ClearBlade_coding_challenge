c3fdf99d5797b8c7bfcc31c9b50f7213d316424e
Rick DeAmicis <rtdeamicis@gmail.com>
2021-04-27 20:39:21 -0700

    initial commit

---
+++ coding_challenge_rtd.py
@@ -0,0 +1,8 @@
+from clearblade.ClearBladeCore import System,Query, Developer
+
+SystemKey = 'c4dfbc880ce891a09fe8eb92eb9d01'
+SystemSecret = 'C4DFBC880CC0D8A8B5FCE5B4DB44'
+
+mySystem = System(SystemKey, SystemSecret)
+
+
