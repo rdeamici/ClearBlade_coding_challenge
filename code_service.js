ded802feefca9ae1549b80d08d947fc52a0a9b5a
rdeamici <rtdeamicis@gmail.com>
2021-05-01 13:12:15 -0700

    code_service.js now simplified and should work as expceted.

--- code_service.js
+++ code_service.js
@@ -14,49 +14,20 @@
 
 function bleDevices(req,resp) {
     log("req = "+JSON.stringify(req))
+    ClearBlade.init({request:req});
+
+    if(!req.params.body){
+		resp.error("Body not passed")
+	}
+	var newDevice = JSON.parse(req.params.body)
+    log("newDevice = "+JSON.stringify(newDevice))
     
-	ClearBlade.init({request:req});
-	var messenger = ClearBlade.Messaging()  
     const collection = "ble_devices"
     var ble_col = ClearBlade.Collection( {collectionName: collection } );
-    const TOPIC = "ble/_platform";  
-	
-    messenger.subscribe(TOPIC, function(err, data) {
-        if(err) {
-            resp.error("subscribe error : " + JSON.stringify(data))
-        }
-        while (true) {
-            messenger.waitForMessage([TOPIC], function(err, msg, topic) {
-                if(err) {
-                    resp.error("message error : " + JSON.stringify(msg));
-                }
-                log("msg = "+ JSON.stringify(msg))
-                log("topic = "+topic)
-                logMsgToCollection(msg,topic)
-            })
-        }
-    })
-
-    function logMsgToCollection(msg, topic) {
-        // DEBUG MESSAGE
-        log("recording message " +JSON.stringify(msg)+ " from "+ topic + " to "+ ble_devices);
-        var newDevice = {
-            time: new Date(),
-            addr: msg.addr
-        };
-        if (msg.name) {
-            newDevice.name = msg.name
-        }
-        log("newDevice = "+JSON.stringify(newDevice))
-        //this inserts the the newDevice item into the collection that col represents
-        ble_col.create(newDevice, function (err, data) {
+    ble_col.create(newDevice, function (err, data) {
             if (err) {
                 resp.error("creation error : " + JSON.stringify(data));
-            } else {
-                resp.success(data);
             }
         })
-    
-	}		  
-    resp.success("Success");
+    resp.success("Successfully added "+JSON.stringify(newDevice)+" to collection " +collection);
 }
