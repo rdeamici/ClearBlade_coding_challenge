32b3de0c3aac0d6bea42088f0c904415d260ff9e
Rick DeAmicis <44657127+rdeamici@users.noreply.github.com>
2021-05-03 13:09:44 -0700

    Update collect_data_and_send_to_clearblade.py
    removed unused libraries, added main() function, other small code cleanups

--- collect_data_and_send_to_clearblade.py
+++ collect_data_and_send_to_clearblade.py
@@ -1,27 +1,29 @@
-import string
 import json
 from time import sleep
 import sysInfo
 
-from bluepy.btle import Scanner, DefaultDelegate
-from clearblade.ClearBladeCore import System, Query, Developer
+from clearblade.ClearBladeCore import System
 
+# creates and returns two variables to hold messages 
+# to be published to ClearBlade platform
 def create_msgs():
     total, available = sysInfo.ram()
-    processes = sysInfo.processes()
+    num_processes = sysInfo.processes()
     temp = sysInfo.temp()
     bles = sysInfo.ble()
     model, serial = sysInfo.raspberry_model_serial()
     num_bles = len(bles)
+    
     system_overview =  {
         'total_ram': total,
         'available_ram': available,
-        'number_of_running_processes':processes,
+        'number_of_running_processes':num_processes,
         'device_temperature': temp,
         'number_ble_in_range': num_bles,
         "raspberry_model":model,
         "raspberry_serial_num":serial
     }
+    
     bluetooth_devices = []
     for ble in bles:
         msg = {
@@ -35,26 +37,30 @@ def create_msgs():
 
     return system_overview, bluetooth_devices
 
-#send message to Broker
-SystemKey = 'c4dfbc880ce891a09fe8eb92eb9d01'
-SystemSecret = 'C4DFBC880CC0D8A8B5FCE5B4DB44'
-admin_email = 'rtdeamicis@gmail.com'
-admin_pw = 'H1r3m3pls'
 
-mySystem = System(SystemKey, SystemSecret)
-admin = mySystem.User(admin_email, admin_pw)
+def main():
+    #send message to ClearBlade platform
+    SystemKey = 'c4dfbc880ce891a09fe8eb92eb9d01'
+    SystemSecret = 'C4DFBC880CC0D8A8B5FCE5B4DB44'
+    admin_email = 'rtdeamicis@gmail.com'
+    admin_pw = 'H1r3m3pls'
 
-mqtt = mySystem.Messaging(admin)
-sys_overview, bles = create_msgs()
+    mySystem = System(SystemKey, SystemSecret)
+    admin = mySystem.User(admin_email, admin_pw)
 
-mqtt.connect()
+    mqtt = mySystem.Messaging(admin)
+    sys_overview, bles = create_msgs()
 
-for ble in bles:
-    print(json.dumps(ble)) # debug print statement
-    mqtt.publish('ble/_platform',json.dumps(ble))
-    sleep(1)
+    mqtt.connect()
 
-mqtt.publish('sysinfo',json.dumps(sys_overview))
+    for ble in bles:
+        print(json.dumps(ble)) # debug print statement
+        mqtt.publish('ble/_platform',json.dumps(ble))
+        sleep(1)
 
-mqtt.disconnect()
+    mqtt.publish('sysinfo',json.dumps(sys_overview))
 
+    mqtt.disconnect()
+
+if __name__ == "__main__":
+    main()
