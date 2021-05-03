5f115dd0aeab5333644a482ab194c71e2cf6f4da
Rick DeAmicis <44657127+rdeamici@users.noreply.github.com>
2021-05-03 13:13:10 -0700

    Update bleDevices.js
    cleaned up code a bit fo readability

--- bleDevices.js
+++ bleDevices.js
@@ -16,20 +16,24 @@ function bleDevices(req,resp) {
     log("req = "+JSON.stringify(req))
     ClearBlade.init({request:req});
 
-    if(!req.params.body){
-		resp.error("Body not passed")
-	}
-	var newDevice = JSON.parse(req.params.body)
+    if(!req.params.body) {
+        resp.error("Body not passed")
+    }
+    var newDevice = JSON.parse(req.params.body)
     newDevice.date = new Date()
+    
     log("newDevice = "+JSON.stringify(newDevice))
     
-    const collection = "de83e2880ca4b886938cfbbbac2c"
-    var ble_col = ClearBlade.Collection( {collectionID: collection } );
+    const ble_collection = "de83e2880ca4b886938cfbbbac2c"
+    var ble_col = ClearBlade.Collection( {collectionID: ble_collection } );
+
     ble_col.create(newDevice, function (err, data) {
-            if (err) {
-                resp.error("creation error : " + JSON.stringify(data));
-            }
-            log("data added to "+collection+" = "+data)
-        })
-    resp.success("Successfully added "+JSON.stringify(newDevice)+" to collection " +collection);
+        if (err) {
+            resp.error("creation error : " + JSON.stringify(data));
+        }
+
+	log("data added to "+collection+" = "+data)
+    })
+
+    resp.success("Successfully added "+JSON.stringify(newDevice)+" to collection "+ble_collection);
 }
